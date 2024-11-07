e03efcf686c7d57461665385e28dec9c1216f22c
Peter Joseph Buddendeck <pjbuddendeck@wm.edu>
2024-11-07 10:41:31 -0500

    Completed assignment

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
 
+function addTask(){
+    /*here we need to access the content of the input and do something with it*/
+    const taskInput = document.getElementById('taskInput').value;
+
+    if (taskInput){
+        const newTask = document.createElement('li');
+        newTask.innerText= taskInput;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className="button";
+        deleteButton.innerText="x";
+        newTask.appendChild(deleteButton)
+
+        deleteButton.addEventListener("click", () => taskList.removeChild(newTask))
+
+        const taskList = document.getElementById('taskList');
+        taskList.appendChild(newTask);
+    }
+}
